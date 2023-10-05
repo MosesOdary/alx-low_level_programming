d2bbab6e80d9e23376397c65a94a9383efa64f35
MosesOdary <mosesodary101@gmail.com>
2023-10-05 05:48:55 +0000

    Added betty's conventions

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Program entry point
+ *
+ * Description - Prints "Programming is like building a multilingual puzzle
+ *
+ * Return: 0 - Success
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return 0;
+	return (0);
 }
