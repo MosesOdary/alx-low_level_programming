f342dc08f3b716eb8bc6ea4a4f3471feee91a631
MosesOdary <mosesodary101@gmail.com>
2023-10-05 06:29:45 +0000

    Extra tasks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,12 +6,12 @@
  *
  * Desription - Writes to the standard error
  *
- * Return: 0 - Success
+ * Return: -1 - Failure
  */
 
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-	return (0);
+	return (-1);
 }
