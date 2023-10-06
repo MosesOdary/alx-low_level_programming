682d1291306288ffb956f23f5514765cf2601013
MosesOdary <mosesodary101@gmail.com>
2023-10-06 21:41:43 +0000

    Added Betty's conventions

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,30 +7,31 @@
 
 int main(void)
 {
-        int i = 0;
-        int j = 0;
+	int i = 0;
+	int j = 0;
 
-        for (; i < 100; i++)
-        {
-                for (j = i; j < 100; j++)
-                {
-                        if (j != i)
-                        {
-                                putchar(i / 10 + '0');
-                                putchar(i % 10 + '0');
-                                putchar(' ');
-                                putchar(j / 10 + '0');
-                                putchar(j % 10 + '0');
+	for (; i < 100; i++)
+	{
+		for (j = i; j < 100; j++)
+		{
+			if (j != i)
+			{
+				putchar(i / 10 + '0');
+				putchar(i % 10 + '0');
+				putchar(' ');
+				putchar(j / 10 + '0');
+				putchar(j % 10 + '0');
 
-                                if (((i * 100) + j) != 9899)
-                                {
-                                        putchar(',');
-                                        putchar(' ');
-                                }
-                        }
-                }
-        }
-        putchar('\n');
+				if (((i * 100) + j) != 9899)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
 
-        return (0);
+	putchar('\n');
+
+	return (0);
 }
