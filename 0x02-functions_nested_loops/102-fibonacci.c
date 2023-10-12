661de82cf91e42a0850aff211a7eccd560a2a3ff
MosesOdary <mosesodary101@gmail.com>
2023-10-12 01:47:11 +0000

    Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 - success
+ */
+
+int main(void)
+{
+	int i = 0;
+	long int fibonacciArray[50] = {1, 2};
+
+	for (i = 2; i < 50; i++)
+		fibonacciArray[i] = fibonacciArray[i - 2] + fibonacciArray[i - 1];
+
+	for (i = 0; i < 50; i++)
+		printf("%ld, ", fibonacciArray[i]);
+
+	return (0);
+}
