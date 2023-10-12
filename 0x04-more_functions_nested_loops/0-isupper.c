75590db4f10e2147985afbeebcdb6e3c39bbe8eb
MosesOdary <mosesodary101@gmail.com>
2023-10-13 00:28:15 +0300

    Bug fixes

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,15 +2,15 @@
 
 /**
  * _isupper - Function that checks for uppercase character
- * 
+ *
  * @c: The character being tested
- * 
+ *
  * Return: 1 if uppercase and 0 otherwise
 */
 
 int _isupper(int c)
 {
-    if (c > 97)
+	if (c > 97)
 		return (0);
 	else
 		return (1);
