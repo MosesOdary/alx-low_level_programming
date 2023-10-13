d78125922305938e9c44c53cabcd122251d03e55
MosesOdary <mosesodary101@gmail.com>
2023-10-13 03:24:23 +0300

    print a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - print a line
+ * @n: The space in characters occupied by the line
+ * Return: Always 0.
+ */
+
+
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n < 1)
+		_putchar('\n');
+	else
+		for (; i <= n; i++)
+		{
+			_putchar('_');
+		}
+}
+
+
