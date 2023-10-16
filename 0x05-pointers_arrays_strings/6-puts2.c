95f19224e4bae52b3eff42ec28ce4afcf872d07a
MosesOdary <mosesodary101@gmail.com>
2023-10-17 00:09:39 +0300

    Print a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * puts2 - Print a string
+ *
+ * @s: The string whose length is beib=ng checked
+ *
+ * Return: void
+*/
+
+void puts2(char *str)
+{
+    int i = 0;
+    int stringLength = _strlen(str);
+
+    for (; i < stringLength; i += 2)
+        _putchar(str[i]);
+}
+
+
+/**
+ * _strlen - Return the length of a string
+ *
+ * @s: The string whose length is beib=ng checked
+ *
+ * Return: void
+*/
+
+int _strlen(char *s)
+{
+	const char *end = s;
+
+	for (; *end != '\0'; ++end)
+		;
+	return (end - s);
+}
+
