3021241720887f12df453f538ed4a7e2ae4fa68f
MosesOdary <mosesodary101@gmail.com>
2023-10-16 06:43:31 +0300

    Initial commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Function that updates a value to 98
+ *
+ * @n: pointer to value to be modified
+ *
+ * Return: void
+*/
+
+void reset_to_98(int *n)
+{
+    *n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ *
+ * Desc: Header file containing declartions for functions
+ */
+
+int _putchar(char ch);
+void reset_to_98(int *n);
+
+#endif
