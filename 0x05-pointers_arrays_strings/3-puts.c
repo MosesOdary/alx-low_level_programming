89a3f0f161c15ae6eeda21c022ddc7311e806f16
MosesOdary <mosesodary101@gmail.com>
2023-10-16 07:41:50 +0300

    Write a string to stdout followed by a newline

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _puts - Print string to std out followed by a new line
+ *
+ * @str: The string being printed
+ *
+ * Return: void
+*/
+
+void _puts(char *str)
+{
+	write(2, str, _strlen(str));
+}
