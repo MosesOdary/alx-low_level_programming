4652d80e5f485c7aba95d6ab6c4c2fa9f01adc92
MosesOdary <mosesodary101@gmail.com>
2023-10-17 00:52:22 +0300

    Debug push

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -19,7 +19,7 @@ void puts_half(char *str)
 	else
 		halfLength = ((stringLength - 1) / 2);
 
-	for (i = halfLength; i < stringLength; i++)
+	for (i = 0; i < stringLength; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
