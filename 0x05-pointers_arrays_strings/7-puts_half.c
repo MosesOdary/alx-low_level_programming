e70387a280c1ea4db0c515e3b5f85df7a0c817c2
MosesOdary <mosesodary101@gmail.com>
2023-10-17 00:48:05 +0300

    Bug fix

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,14 +12,14 @@ void puts_half(char *str)
 {
 	int stringLength = _strlen(str);
 	int halfLength = 0;
-	int i = halfLength;
+	int i = 0;
 
 	if ((stringLength % 2) == 0)
 		halfLength = stringLength / 2;
 	else
 		halfLength = ((stringLength - 1) / 2);
 
-	for (; i < stringLength; i++)
+	for (i = halfLength; i < stringLength; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
