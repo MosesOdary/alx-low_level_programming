8d606dbe85d9894e38505b56e116bf170abd5ca1
MosesOdary <mosesodary101@gmail.com>
2023-10-17 02:20:58 +0300

    Copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copy a string from src to dest
+ *
+ * @dest: destination string
+ * @src: src string
+ *
+ * Return: void
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	int stringLength = _strlen(src);
+
+	for (; i < stringLength; i++)
+	{
+		dest[i] = src[i];
+	}
+}
+
+/**
+ * _strlen - Return the length of a string
+ *
+ * @s: The string whose length is being checked
+ *
+ * Return: void
+*/
+
+int _strlen(char *s)
+{
+	const char *end = s;
+
+	for (; *end != '\0'; ++end)
+		;
+	return (end - s);
+}
+
+
