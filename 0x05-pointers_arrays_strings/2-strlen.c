0938d51c8368e7cdfd64090aa9eaeddc25fba492
MosesOdary <mosesodary101@gmail.com>
2023-10-16 07:25:26 +0300

    Return the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Return the length of a string
+ *
+ * @s: The string whose length is beib=ng checked 
+ *
+ * Return: void
+*/
+
+int _strlen(char *s)
+{
+	const char* end = s;
+	for( ; *end != '\0'; ++end)
+		;
+	return end - s;
+}
