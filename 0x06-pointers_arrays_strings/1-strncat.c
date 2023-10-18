7331b8dd77cf8df28ac675d000c96938bf19e9c5
MosesOdary <mosesodary101@gmail.com>
2023-10-18 23:50:52 +0300

    concatenate string up to n characters

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,7 +13,20 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
+	int i, j = 0;
+	int destLength = strlen(dest);
 
+	j = destLength;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[j++] = src[i];
+	}
+
+	dest[j] = '\0';
+
+	return (dest);
 }
+
 
 
