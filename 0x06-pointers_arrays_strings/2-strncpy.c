4ecaa22a388f16b06525f711a294f699ed4a528b
MosesOdary <mosesodary101@gmail.com>
2023-10-19 00:25:29 +0300

    Copy characters from one string to another

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,6 +13,16 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
+	int i = 0;
+	int destLength = strlen(dest);
+	int srcLength = strlen(src);
 
+	for (; i < n && i < srcLength; i++)
+		dest[i] = src[i];
+
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
 }
 
