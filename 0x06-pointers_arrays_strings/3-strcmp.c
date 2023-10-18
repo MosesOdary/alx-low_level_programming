17d268b27ae3c7bd164b23713f623aaccc0b3553
MosesOdary <mosesodary101@gmail.com>
2023-10-19 00:34:31 +0300

    Compare 2 strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,6 +11,32 @@
 
 int _strcmp(char *s1, char *s2)
 {
+	int i = 0;
+	int result = 0;
+	unsigned int true = 1;
 
+	while (true)
+	{
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+		else if (s1[i] == '\0')
+		{
+			result = s1[i];
+			break;
+		}
+		else if (s2[i] == '\0')
+		{
+			result = s1[i];
+			break;
+		}
+		else if (s1[i] != s2[i])
+		{
+			result = s1[i] - s2[i];
+			break;
+		}
+		else
+			i++;
+	}
+	return (result);
 }
 
