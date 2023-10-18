cad5d3f1f89dd69b552a5bc47b7cda44842466d6
MosesOdary <mosesodary101@gmail.com>
2023-10-19 02:11:41 +0300

    Encode a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,8 +12,8 @@ char *leet(char *str)
 {
 	const unsigned char arraySize = 5;
 	char *cp = str;
-	char key[arraySize] = {'A', 'E', 'O', 'T', 'L'};
-	int value[arraySize] = {4, 3, 0, 7, 1};
+	char key[] = {'A', 'E', 'O', 'T', 'L'};
+	int value[] = {4, 3, 0, 7, 1};
 	unsigned int i = 0;
 
 	while (*str)
