3182b0cd6b1dea23569357a7b9616f10d46e50de
MosesOdary <mosesodary101@gmail.com>
2023-10-23 20:28:30 +0300

    fill n bytes of memory area with constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _memset - fills first n bytes of memory area with the constant byte b
+* _memset - fill first n bytes of memory area with the constant byte b
 *
 * @s: buffer to be filled
 * @b: bytes used to fill
@@ -13,7 +13,7 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
+	unsigned int i = 0;
 
 	for (; i < n; i++)
 	{
