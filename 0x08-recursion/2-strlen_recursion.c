986a56243fcecb2864b5b19e2640eaa07fbd647b
MosesOdary <mosesodary101@gmail.com>
2023-10-26 02:13:17 +0300

    Strlen

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -12,13 +12,9 @@
 
 int _strlen_recursion(char *s)
 {
-	uint8_t length = 0;
-
 	if (*s == '\0')
 		return (0);
 	else
-		length += (_strlen_recursion(++s) + 1);
-
-	return (length);
+		return (_strlen_recursion(++s) + 1);
 }
 
