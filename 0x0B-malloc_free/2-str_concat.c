9aea33a8d45497c02bf2bf384b1777e558a316da
MosesOdary <mosesodary101@gmail.com>
2023-11-01 23:59:48 +0300

    Bug fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,32 +12,35 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *newString = NULL;
-	uint16_t s1Length = strlen(s1);
-	uint16_t s2Length = strlen(s2);
-	uint8_t i, j = 0;
+	uint16_t s1Length = 0;
+	uint16_t s2Length = 0;
+	uint8_t i = 0, j = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+	for (s1Length = 0; *(s1 + s1Length); s1Length++)
+	{
+	}
+	for (s2Length = 0; *(s2 + s2Length); s2Length++)
+	{
+	}
 
 	newString = malloc(sizeof(char) * (s1Length + s2Length + 1));
-	
 	if (newString == NULL)
 		return (NULL);
-
 	for (i = 0; i < (s1Length + s2Length); i++)
 	{
 		if (i < s1Length)
-			*(newString + i) = *(s1 + i);
+			newString[i] = s1[i];
 
 		else
-		{
-			*(newString + i) = *(s2 + j);
-			j++;
-		}
-	}
-	*(newString + i) = '\0';
+			newString[i] = s2[j++];
 
-	return (newString);
+	}
+	newString[i] = '\0';
+	s1 = newString;
+	
+	return (s1);
 }
