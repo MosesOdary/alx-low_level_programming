480dd195c66b10e9d65cff99b2f6b414304c533f
MosesOdary <mosesodary101@gmail.com>
2023-11-06 17:20:45 +0300

    Bug fix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 #include "dog.h"
 
 /**
@@ -11,7 +13,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d)
+	if (d != NULL)
 	{
 		d->name = name;
 		d->age = age;
