fb67a136385d95f7172b2686c7978e4fea09532d
MosesOdary <mosesodary101@gmail.com>
2023-11-06 22:58:47 +0300

    Print a dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,11 +12,20 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		if (!(d->name))
+		{
 			printf("Name: (nil)\nAge: %f\nOwner: %s\n", d->age, d->owner);
+			return;
+		}
 		if (!(d->age))
+		{
 			printf("Name: %s\nAge: (nil)\nOwner: %s\n", d->name, d->owner);
+			return;
+		}
 		if (!(d->owner))
+		{
 			printf("Name: %s\nAge: %f\nOwner: (nil)\n", d->name, d->age);
+			return;
+		}
 
 		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 	}
