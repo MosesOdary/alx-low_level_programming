7d40a1b032e2050eb73902c68ceaf60ad2fd41df
MosesOdary <mosesodary101@gmail.com>
2023-11-08 23:28:45 +0300

    Bug fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,33 +16,33 @@
 
 int main(int argc, char *argv[])
 {
-	int32_t num1, num2;
+	int32_t num1 = 0;
+	int32_t num2 = 0;
 	char *operator;
 
-	operator = argv[2];
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	if ((*operator != '+' && *operator != '-' && *operator != '*'
-		&& *operator != '/' && *operator != '%') || operator[1] != '\0')
+	num1 = atoi(argv[1]);
+	operator = argv[2];
+	num2 = atoi(argv[3]);
+
+	if (!get_op_func(operator) || operator[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-
-	if ((*operator == '/' && num2 == 0) ||
-		(*operator == '%' && num2 == 0))
+	if ((*operator == '/' && num2 == 0) || (*operator == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-
+	
 	printf("%d\n", get_op_func(operator)(num1, num2));
+
 	return (0);
 }
