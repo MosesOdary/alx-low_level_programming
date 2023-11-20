51c98851f6aa62cd346114497fc48021c124bbd1
MosesOdary <mosesodary101@gmail.com>
2023-11-20 23:10:11 +0300

    More bug fixes

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -5,25 +5,28 @@
  *
  * @head: list to print
  *
- * Return: number of nodes in list
+ * Return: nodesber of nodes in list
 */
 
 size_t print_listint_safe(const listint_t *head)
 {
-	uint32_t num = 0;
-	int64_t diff = 0;
+	uint32_t nodes = 0;
+	int64_t diff;
 
 	while (head)
 	{
+		nodes++;
+	
 		diff = head - head->next;
 		printf("[%p] %d\n", (void *)head, head->n);
-
+	
 		if (diff > 0)
 			head = head->next;
 		else
+		{
 			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-
-		num++;
+			break;
+		}
 	}
-	return (num);
+	return (nodes);
 }
