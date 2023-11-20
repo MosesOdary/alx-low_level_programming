e4326e30cba7738b09deb523ed04885db04a7a01
MosesOdary <mosesodary101@gmail.com>
2023-11-20 23:05:37 +0300

    Extra task

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints a list
+ *
+ * @head: list to print
+ *
+ * Return: number of nodes in list
+*/
+
+size_t print_listint_safe(const listint_t *head)
+{
+	uint32_t num = 0;
+	int64_t diff = 0;
+
+	while (head)
+	{
+		diff = head - head->next;
+		printf("[%p] %d\n", head, head->n);
+
+		if (diff > 0)
+			head = head->next;
+		else
+			printf("-> [%p] %d\n", head->next, head->next->n);
+
+		num++;
+	}
+	return (num);
+}
