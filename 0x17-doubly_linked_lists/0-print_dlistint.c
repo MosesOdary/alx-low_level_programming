c9461505aa143d9af431eca6e8f619668dce8e9e
MosesOdary <mosesodary101@gmail.com>
2024-01-11 23:25:26 +0300

    Task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,8 +1,31 @@
 #include "lists.h"
 
-
+/**
+ * print_dlistint - Prints all the elements of an int list
+ *
+ * @h: The list being printed
+ *
+*/
 size_t print_dlistint(const dlistint_t *h)
 {
+	uint16_t i = 0;
+	dlistint_t* this = NULL;
 
+	if (!h)
+		return -1;
+
+	for (; h ->next; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+
+	}
+
+	printf("%d\n", h->n);
+	h = h->next;
+	i++;
+
+	return i;
 }
+
 
