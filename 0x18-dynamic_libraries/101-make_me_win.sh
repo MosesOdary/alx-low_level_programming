77ca750d5ca837c852971e1580f56fab6f7450e3
MosesOdary <mosesodary101@gmail.com>
2024-01-15 14:49:11 +0300

    Task 4 (advanced) - minor fix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 wget -P /tmp/ https://github.com/MosesOdary/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libgm.so
-export LD_PRELOAD =/tmp/libtest.so
+export LD_PRELOAD =/tmp/libgm.so
