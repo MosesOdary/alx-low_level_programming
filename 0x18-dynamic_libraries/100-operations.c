c53511c92d83779d1ccfc04a309f3b1cba2d1f9f
MosesOdary <mosesodary101@gmail.com>
2024-01-15 14:25:26 +0300

    Task 3 (advanced)

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,29 @@
+
+
+
+long long add(long long a, long long b)
+{
+	return (a + b);
+}
+
+long long sub(long long a, long long b)
+{
+	return (a - b);
+}
+
+long long mul(long long a, long long b)
+{
+	return (a * b);
+}
+
+long long div(long long a, long long b)
+{
+	return (a / b);
+}
+
+long long mod(long long a, long long b)
+{
+	return (a % b);
+}
+
+
