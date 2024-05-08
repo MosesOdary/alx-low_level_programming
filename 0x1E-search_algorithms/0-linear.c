51af9b8c5f8d6e7ce293d2b86a07d45ab665059f
MosesOdary <mosesodary101@gmail.com>
2024-05-09 00:36:00 +0300

    Bug fix #4

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,21 +13,16 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
-	if (!array)
+	if (array != NULL)
 	{
-		return (-1);
-	}
-
-	for (; i < size; i++)
-	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-
-		if (array[i] == value)
+		for (i = 0; i < size; i++)
 		{
-			return (i);
+			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+			{
+				return (i);
+			}
 		}
-		array++;
 	}
-
 	return (-1);
 }
