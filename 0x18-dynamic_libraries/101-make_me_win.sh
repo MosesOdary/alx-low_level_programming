52564edb60bedb5732ce55a52323d27ba5322929
odarym <mosesodary101@gmail.com>
2024-07-26 14:12:01 +0300

    Bug fix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-wget -P /tmp/ https://github.com/MosesOdary/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libgm.so
-export LD_PRELOAD =/tmp/libgm.so
+wget -P /tmp/ https://github.com/dennisnderitu254/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libtest.so
+export LD_PRELOAD =/tmp/libtest.so
